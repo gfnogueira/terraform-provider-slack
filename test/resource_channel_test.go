920d12cbd27cece01784aacd68aa2b0012154d5c
Guilherme Nogueira <nogueira.gfn@gmail.com>
2025-06-16 00:04:09 -0300

    Add provider tests

---
+++ test/resource_channel_test.go
@@ -0,0 +1,35 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestSlackChannel_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			if v := os.Getenv("SLACK_TOKEN"); v == "" {
+				t.Skip("SLACK_TOKEN must be set")
+			}
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: `
+provider "slack" {
+  token = "` + os.Getenv("SLACK_TOKEN") + `"
+}
+
+resource "slack_channel" "test" {
+  name       = "tf-test-channel"
+  is_private = false
+}
+`,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("slack_channel.test", "name", "tf-test-channel"),
+					resource.TestCheckResourceAttr("slack_channel.test", "is_private", "false"),
+				),
+			},
+		},
+	})
+}
